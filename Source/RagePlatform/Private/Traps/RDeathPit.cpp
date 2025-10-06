4a9ad0b2700c25f344486a54878ce8094cdb4c08
SakethGran <2506570@uad.ac.uk>
2025-10-06 13:11:56 +0100

    LavaPit

---
+++ Source/RagePlatform/Private/Traps/RDeathPit.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Traps/RDeathPit.h"
+
+#include "Components/BoxComponent.h"
+#include "Framework/RGameInstance.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/RageCharacter.h"
+
+// Sets default values
+ARDeathPit::ARDeathPit()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("DefaultSceneRoot"));
+
+	DeathPitMesh = CreateDefaultSubobject<UStaticMeshComponent>("DeathPit");
+	DeathPitMesh->SetupAttachment(GetRootComponent());
+
+	DeathTrigger = CreateDefaultSubobject<UBoxComponent>("DeathTrigger");
+	DeathTrigger->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void ARDeathPit::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GameInstance = Cast<URGameInstance>(UGameplayStatics::GetGameInstance(this));
+	
+	DeathTrigger->OnComponentBeginOverlap.AddDynamic(this, &ARDeathPit::OnComponentBeginOverlapKillBox);
+}
+
+void ARDeathPit::OnComponentBeginOverlapKillBox(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (Cast<ARageCharacter>(OtherActor))
+	{
+		GameInstance->OnDeath.Broadcast();
+	}
+}
+
