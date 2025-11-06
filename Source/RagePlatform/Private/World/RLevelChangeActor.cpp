f9db88129acfc31f5705051418da040eab41db81
SakethGran <234590502+SakethGran@users.noreply.github.com>
2025-11-06 12:04:24 +0000

    Function Deleted

--- Source/RagePlatform/Private/World/RLevelChangeActor.cpp
+++ Source/RagePlatform/Private/World/RLevelChangeActor.cpp
@@ -53,7 +53,6 @@ void ARLevelChangeActor::OnComponentBeginOverlap(UPrimitiveComponent* Overlapped
 
 	if (Cast<ARageCharacter>(OtherActor))
 	{
-		OnLevelStartChange();
 
 		GetWorld()->GetTimerManager().SetTimer(
 			FadeHandle,
