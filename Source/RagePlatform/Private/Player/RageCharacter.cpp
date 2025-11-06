126782d3d537e2477ca8685ff5a485183d801b6b
SakethGran <234590502+SakethGran@users.noreply.github.com>
2025-11-06 13:02:06 +0000

    Resolving Issues

--- Source/RagePlatform/Private/Player/RageCharacter.cpp
+++ Source/RagePlatform/Private/Player/RageCharacter.cpp
@@ -162,8 +162,8 @@ void ARageCharacter::PauseGame()
 		{
 			PauseWidgetRef->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
 			PlayerController->SetShowMouseCursor(true);
-			const FInputModeGameAndUI InputModeGameAndUI;
-			PlayerController->SetInputMode(InputModeGameAndUI);
+			const FInputModeUIOnly InputModeUIOnly;
+			PlayerController->SetInputMode(InputModeUIOnly);
 
 			const int32 Index = UKismetMathLibrary::RandomIntegerInRange(0,PauseTexts.Num() - 1);
 			PauseWidgetRef->PauseText->SetText(PauseTexts[Index]);
